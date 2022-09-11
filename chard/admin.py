054832a242a9ed6219d7e0c889b1b031f21daea1
James Potter <james.potter@gmail.com>
2022-09-11 18:42:22 +0800

    default Task admin ordering

--- chard/admin.py
+++ chard/admin.py
@@ -6,6 +6,7 @@ from .models import Task
 class TaskAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "status", "created_at", "updated_at")
     search_fields = ("id", "name")
+    ordering = ("-created_at",)
 
 
 admin.site.register(Task, TaskAdmin)
